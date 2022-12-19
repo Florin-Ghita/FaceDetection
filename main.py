7cb13c636a4c8c95847dc0aed0e2c475aaa542b0
Ghita Florin <ghitafm94@gmail.com>
2022-12-19 21:18:58 +0200

    Implemented face recognition and counter

--- main.py
+++ main.py
@@ -8,3 +8,30 @@ layout = [
 
 window = sg.Window('Face Detector', layout)
 
+#get video
+video = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+while True:
+    event, values = window.read(timeout = 0)
+    if event == sg.WIN_CLOSED:
+        break
+    _, frame = video.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.3,
+        minNeighbors=7,
+        minSize =(50,50))
+    print(faces)
+
+    #Update image
+    imgbytes = cv2.imencode('.png', frame)[1].tobytes()
+    window['-IMAGE-'].update(data=imgbytes)
+
+    #Update text
+
+    window['-TEXT-'].update(f'People in picture: {len(faces)}')
+
+window.close()
+
