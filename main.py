d61fa522895df1248efa7c1c3401f7c786355ad5
Ghita Florin <ghitafm94@gmail.com>
2022-12-19 20:10:11 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import PySimpleGUI as sg
+import cv2
+
+layout = [
+    [sg.Image(key='-IMAGE-')],
+    [sg.Text('People in picture: 0', key='-TEXT-', expand_x=True, justification='c')]
+]
+
+window = sg.Window('Face Detector', layout)
+
